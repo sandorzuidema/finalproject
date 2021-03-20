20e3026c33a076b410397f85ff3055f4fa4c515e
sandorzuidema <78413043+sandorzuidema@users.noreply.github.com>
2021-03-20 12:53:30 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # In order to succesfully execute this shell script the shell script should be run on a RUG computer through linux bash.
 # More explanation about the commands is available in the research paper in the github.
-# Any broken pipe errors can be ignored.
+
 
 # 1. This function navigates to the map containing the 2021 februari tweets using the command cd.
 function navigator {
@@ -16,7 +16,7 @@ function navigator {
 function randstad_left {
 	echo This may take 30/60 minutes, depending on your processing power.
 	echo The amount of mentionings of political left parties in the Randstad for the februari 2021 is:
-	zless /net/corpora/twitter2/Tweets/2021/02/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location | grep -w -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland'| grep -iw -e  'PvdD' -e 'GL' -e 'PvdA' -e 'SP' -e 'CU' -e 'D66' -e '50-plus' -e 'PVV' | wc -l 	
+	zless /net/corpora/twitter2/Tweets/2021/02 *.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location | grep -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' |grep -iw -e  'PvdD' -e 'GL' -e 'PvdA' -e 'SP' -e 'CU' -e 'D66' -e '50-plus' -e 'PVV' | wc -l 
 	echo
 }
 #3. This function calculates the amount of tweets mentioning a political right party in the Randstad in februari 2021.
@@ -27,7 +27,9 @@ function randstad_left {
 function randstad_right {
 	echo This may take 30/60 minutes, depending on your processing power.
 	echo The amount of mentionings of political right parties in the Randstad for the februari 2021 is:
-	zless /net/corpora/twitter2/Tweets/2021/02/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location | grep -w -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland'| grep -iw -e  'CDA' -e 'SGP' -e 'VVD' -e 'JA21' -e 'FvD' | wc -l	
+	zless /net/corpora/twitter2/Tweets/2021/02 *.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location \
+	grep -w -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' \
+	grep -iw -e  'CDA' -e 'SGP' -e 'VVD' -e 'JA21' -e 'FvD' | wc -l	
 	echo
 }
 #4. This function calculates the amount of tweets mentioning a political left party outside the Randstad in februari 2021.
@@ -39,7 +41,9 @@ function randstad_right {
 function non_randstad_left {
 	echo This may take 30/60 minutes, depending on your processing power.
 	echo The amount of mentionings of political left parties outside the Randstad for the februari 2021 is:
-	zless /net/corpora/twitter2/Tweets/2021/02/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location | grep ', Nederland'| grep -v -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' |grep -iw -e  'PvdD' -e 'GL' -e 'PvdA' -e 'SP' -e 'CU' -e 'D66' -e '50-plus' -e 'PVV'| wc -l
+	zless /net/corpora/twitter2/Tweets/2021/02 *.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location \
+	grep ', Nederland'| grep -v -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' \
+	grep -iw -e  'PvdD' -e 'GL' -e 'PvdA' -e 'SP' -e 'CU' -e 'D66' -e '50-plus' -e 'PVV' | wc -l
 	echo
 }
 #5. This function calculates the amount of tweets mentioning a political right party outside the Randstad in februari 2021.
@@ -51,7 +55,9 @@ function non_randstad_left {
 function non_randstad_right {
 	echo This may take 30/60 minutes, depending on your processing power.
 	echo The amount of mentionings of political right parties outside the Randstad for the februari 2021 is:
-	zless /net/corpora/twitter2/Tweets/2021/02/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location | grep ', Nederland'| grep -v -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' |grep -iw -e  'CDA' -e 'SGP' -e 'VVD' -e 'JA21' -e 'FvD' | wc -l	
+	zless /net/corpora/twitter2/Tweets/2021/02 *.gz  | /net/corpora/twitter2/tools/tweet2tab -i text user.location \
+	grep ', Nederland'| grep -v -e 'Amsterdam, Nederland' -e 'Rotterdam, Nederland' -e 'Den Haag, Nederland' \
+	grep -iw -e  'CDA' -e 'SGP' -e 'VVD' -e 'JA21' -e 'FvD' | wc -l	
 	echo
 }
 navigator
